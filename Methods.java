f7e91003625f011d7b4502ba8203774f26e25f05
HarshDev2003 <harshkumardewangan2003@gmail.com>
2025-03-16 20:17:19 +0530

    Methods Basics

---
+++ Methods.java
@@ -0,0 +1,27 @@
+// Methods
+
+// public class Methods{
+//      void print(){
+//         System.out.println("Hello World Java");
+
+
+//     }
+//     public static void main(String args []){
+//         Methods r = new Methods();
+//         r.print();
+//     }
+// }
+
+
+// public class Methods{
+//     public static void print(){
+//         System.out.println("Hello JAva");
+//     }
+//     public static void main (String [] args){
+//         Methods.print();
+
+//     }
+// }
+
+
+
